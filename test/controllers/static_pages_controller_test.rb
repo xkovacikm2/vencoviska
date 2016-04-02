e0756487667d7defbee79f87d99b03a5e920daec
Michal Kovacik <hocikto19@ynet.sk>
2016-04-02 20:35:00 +0200

    full layout finished

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "O nás | Venčoviská"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Kontakt | Venčoviská"
+  end
 end
