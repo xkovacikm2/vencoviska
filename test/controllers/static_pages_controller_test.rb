28600dfaef086d1a4006a2f52e3aa93b206c2b9f
Michal Kovacik <hocikto19@ynet.sk>
2016-04-02 20:02:10 +0200

    bootstrap implemented + hotfix

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Domov | Venčoviská"
+    assert_select "title", "Venčoviská"
   end
 
   test "should get help" do
