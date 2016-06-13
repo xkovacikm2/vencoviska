5b150e57c7c2992f467e029a99add4fbaf02b569
Michal Kovacik <xkovacikm2@gmail.com>
2016-06-13 18:23:04 +0200

    cities db seed from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,10 @@
-User.create!(name:  "Example User",
-             email: "example@example.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
+require 'csv'
 
-10.times do |n|
-  City.create! name: "city n.#{n+1}"
-end
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'obce.csv'))
+csv = CSV.parse(csv_text, encoding: 'utf-8')
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@example.org"
-  description = Faker::Lorem.sentence(20)
-  password = "password"
-  city_id = (n%10)+1
-  User.create!(name:  name,
-               email: email,
-               description: description,
-               password:              password,
-               password_confirmation: password,
-               city_id: city_id,
-               birthday: Time.at(rand * (100.years.ago.to_f - Time.now.to_f)))
-end
-
-City.order(name: :asc).take(10).each do |city|
-  32.times do |n|
-    id= n+1
-    city.areas.create! name: "zona #{n+1}", user_id: id
-  end
-end
-
-users = User.order(:created_at).take(6)
-50.times do |n|
-  content = Faker::Lorem.sentence(5)
-  users.each do |user|
-    user.comments.create! content: content, area_id: n+1
-  end
-end
-
-Area.all.each_with_index do |area, index|
-  (index%5).times do |i|
-    area.area_border_lines.create! area_id: area.id
-  end
+csv.each do |row|
+  city = City.new
+  city.name = row[1]
+  city.save
 end
