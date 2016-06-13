b13f78bd3b84324c04f6663604d1e9050993067a
Michal Kovacik <xkovacikm2@gmail.com>
2016-06-13 18:04:24 +0200

    heroku setting

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
 if ENV["REDISCLOUD_URL"]
-  $redis = Redis.new(:url => 'redis://rediscloud:rVtn7Zs8RI80Eidt@pub-redis-14040.us-east-1-3.7.ec2.redislabs.com:14040')
+  $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
